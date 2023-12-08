c68d1558b04125da864c9ddd0154d1534a494ea7
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2023-12-08 12:53:52 +0100

    Update index.js

--- Routers/index.js
+++ Routers/index.js
@@ -7,10 +7,6 @@ const storyRoute = require("./story")
 const userRoute = require("./user")
 const commentRoute = require("./comment")
 
-res.setHeader('Access-Control-Allow-Origin', 'https://kandqcourier.services');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
 router.use("/auth",authRoute)
 router.use("/story",storyRoute)
 router.use("/user",userRoute)
