086163b892bbdac031ef797d705f4db6c447abe0
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2023-12-08 12:38:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express() ;
 
 app.use(express.json())
 app.use(cors({
-    origin: 'https://www.kandqcourier.services',
+    origin: 'https://kandqcourier.services',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }))
