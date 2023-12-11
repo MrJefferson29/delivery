d54871f18a870bc2b8432915e559576c9b480c5f
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2023-12-12 00:46:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ const app = express() ;
 
 app.use(express.json())
 app.use(cors({
-    origin: '*',
+    origin: 'https://kandq-courier-service.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }))
 
 app.use("/",IndexRoute)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://kandq-courier-service.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
